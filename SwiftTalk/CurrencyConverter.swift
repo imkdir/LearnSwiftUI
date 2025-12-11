4a14f6d5a8f0dcc3131dd5dc19e2c1a4e83d6ca6
Dwight <hichengd@gmail.com>
2025-12-11 15:10:28 +0800

    #157 currency converter + tiny networking

--- SwiftTalk/CurrencyConverter.swift
+++ SwiftTalk/CurrencyConverter.swift
@@ -6,10 +6,53 @@
 //
 
 import SwiftUI
+import Observation
+import TinyNetworking
+
+struct FixerData: Codable {
+    var rates: [String: Double]
+}
+
+let baseUrl = "http://data.fixer.io/api"
+let apiKey = "6c03d31807226b92f10012fe61ee5b2e"
+let latest = Endpoint<FixerData>(
+    json: .get,
+    url: URL(string: "\(baseUrl)/latest?access_key=\(apiKey)")!
+)
+
+@Observable
+final class Resource<A> {
+    let endpoint: Endpoint<A>
+    
+    private(set) var value: A?
+    
+    private var dataTask: URLSessionDataTask? {
+        didSet {
+            oldValue?.cancel()
+        }
+    }
+    
+    init(endpoint: Endpoint<A>) {
+        self.endpoint = endpoint
+        reload()
+    }
+    
+    deinit {
+        dataTask = nil
+    }
+    
+    func reload() {
+        dataTask = URLSession.shared.load(endpoint) { [weak self] result in
+            self?.value = try? result.get()
+        }
+    }
+}
 
 struct CurrencyConverter {
     @State private var input: String = "42"
     @State private var currency: String = "USD"
+    
+    private let resource = Resource(endpoint: latest)
     
     private var rate: Double {
         rates[currency, default: 0.0]
@@ -28,26 +71,38 @@ struct CurrencyConverter {
         return f
     }()
     
-    private let rates: [String: Double] = [
-        "USD": 1.17,
-        "GBP": 0.87,
-        "CNY": 8.26
-    ]
+    private var rates: [String: Double] {
+        resource.value?.rates ?? ["USD": 1.17]
+    }
 }
 
 extension CurrencyConverter: View {
     var body: some View {
-        HStack {
-            TextField("input", text: $input)
-                .keyboardType(.decimalPad)
-                .textFieldStyle(.roundedBorder)
-                .frame(width: 100)
-            Text("EUR")
-            Text("=")
-            Text(output).bold()
-            Picker("picker", selection: $currency) {
-                ForEach(rates.keys.sorted(), id: \.self, content: Text.init)
-            }.frame(width: 100)
+        VStack {
+            HStack {
+                TextField("input", text: $input)
+                    .keyboardType(.decimalPad)
+                    .multilineTextAlignment(.trailing)
+                    .textFieldStyle(.roundedBorder)
+                    .fixedSize()
+                Text("EUR")
+                Text("=")
+                Text(output)
+                    .bold()
+                    .multilineTextAlignment(.trailing)
+                Picker("picker", selection: $currency) {
+                    ForEach(rates.keys.sorted(), id: \.self, content: Text.init)
+                }
+                .fixedSize()
+            }
+            Button("Refresh") {
+                resource.reload()
+            }
+            .buttonStyle(.glassProminent)
         }
     }
+}
+
+#Preview {
+    CurrencyConverter()
 }
