788e10ebc51b21b7fb1fbf9decb6ac6506d76960
imkdir <hichengd@gmail.com>
2025-12-26 17:54:26 +0800

    #305 add more algorithm, support combined id & value

--- SwiftTalk/AsyncTimeline.swift
+++ SwiftTalk/AsyncTimeline.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import AsyncAlgorithms
 
 struct Event: Identifiable, Hashable, Sendable {
-    let id: Int
+    var id: EventID = .single(UUID())
     var time: TimeInterval
     let color: Color
     let value: Value
@@ -54,22 +54,28 @@ struct EventNode: View {
 enum Value: Hashable, Sendable {
     case int(Int)
     case string(String)
+    indirect case combined(Value, Value)
+}
+
+indirect enum EventID: Hashable, Sendable {
+    case single(UUID)
+    case combined(EventID, EventID)
 }
 
 private let sampleInt: [Event] = [
-    .init(id: 0, time: 0, color: .red, value: .int(1)),
-    .init(id: 1, time: 1, color: .red, value: .int(2)),
-    .init(id: 2, time: 2, color: .red, value: .int(3)),
-    .init(id: 3, time: 5, color: .red, value: .int(4)),
-    .init(id: 4, time: 8, color: .red, value: .int(5))
+    .init(time: 0, color: .red, value: .int(1)),
+    .init(time: 1, color: .red, value: .int(2)),
+    .init(time: 2, color: .red, value: .int(3)),
+    .init(time: 5, color: .red, value: .int(4)),
+    .init(time: 8, color: .red, value: .int(5))
 ]
 
 private let sampleString: [Event] = [
-    .init(id: 100_0, time: 1.5, color: .blue, value: .string("a")),
-    .init(id: 100_1, time: 2.5, color: .blue, value: .string("b")),
-    .init(id: 100_2, time: 4.5, color: .blue, value: .string("c")),
-    .init(id: 100_3, time: 6.5, color: .blue, value: .string("d")),
-    .init(id: 100_4, time: 7.5, color: .blue, value: .string("e"))
+    .init(time: 1.5, color: .blue, value: .string("a")),
+    .init(time: 2.5, color: .blue, value: .string("b")),
+    .init(time: 4.5, color: .blue, value: .string("c")),
+    .init(time: 6.5, color: .blue, value: .string("d")),
+    .init(time: 7.5, color: .blue, value: .string("e"))
 ]
 
 extension Value: View {
@@ -77,6 +83,11 @@ extension Value: View {
         switch self {
         case .int(let i): Text(i.description)
         case .string(let s): Text(s)
+        case .combined(let lhs, let rhs):
+            HStack(spacing: 0) {
+                lhs
+                rhs
+            }
         }
     }
 }
@@ -113,19 +124,36 @@ struct TimelineView: View {
     }
 }
 
-extension Array where Element == Event {
-    @MainActor
-    func stream(speed: TimeInterval = 1.0) -> AsyncStream<Event> {
+struct TimedEventStream: AsyncSequence, Sendable {
+    typealias Element = Event
+    let events: [Event]
+    let speed: TimeInterval
+    
+    nonisolated func makeAsyncIterator() -> AsyncStream<Event>.Iterator {
         AsyncStream { continuation in
-            sorted().enumerated().forEach { idx, event in
-                Timer.scheduledTimer(withTimeInterval: event.time / speed, repeats: false) { _ in
-                    continuation.yield(event)
-                    if idx == count - 1 {
-                        continuation.finish()
+            let sortedEvents = events.sorted()
+            let task = Task {
+                let start = Date()
+                for event in sortedEvents {
+                    let interval = event.time / speed
+                    let diff = Date().timeIntervalSince(start)
+                    if interval > diff {
+                        try? await Task.sleep(nanoseconds: UInt64((interval - diff) * 1_000_000_000))
                     }
+                    continuation.yield(event)
                 }
+                continuation.finish()
             }
-        }
+            continuation.onTermination = { _ in
+                task.cancel()
+            }
+        }.makeAsyncIterator()
+    }
+}
+
+extension Array where Element == Event {
+    func stream(speed: TimeInterval = 1.0) -> TimedEventStream {
+        TimedEventStream(events: self, speed: speed)
     }
 }
 
@@ -134,23 +162,38 @@ func run(algorithm: Algorithm, _ lhs: [Event], _ rhs: [Event]) async -> [Event] 
     let lhs = lhs.stream(speed: speed)
     let rhs = rhs.stream(speed: speed)
     
+    var result: [Event] = []
+    let start = Date()
+    var interval: TimeInterval {
+        Date().timeIntervalSince(start) * speed
+    }
     switch algorithm {
     case .merge:
-        let merged = merge(lhs, rhs)
-        return await Array(merged)
-    case .chain:
-        var result: [Event] = []
-        let start = Date()
-        for await event in chain(lhs, rhs) {
-            let interval = Date().timeIntervalSince(start) * speed
+        for await event in merge(lhs, rhs) {
             result.append(.init(id: event.id, time: interval, color: event.color, value: event.value))
         }
-        return result
+    case .chain:
+        for await event in chain(lhs, rhs) {
+            result.append(.init(id: event.id, time: interval, color: event.color, value: event.value))
+        }
+    case .zip:
+        for await (e0, e1) in zip(lhs, rhs) {
+            result.append(.init(id: .combined(e0.id, e1.id), time: interval, color: .green, value: .combined(e0.value, e1.value)))
+        }
+    case .combineLatest:
+        for await (e0, e1) in combineLatest(lhs, rhs) {
+            result.append(.init(id: .combined(e0.id, e1.id), time: interval, color: .green, value: .combined(e0.value, e1.value)))
+        }
+    case .adjacentPairs:
+        for await (e0, e1) in lhs.adjacentPairs() {
+            result.append(.init(id: .combined(e0.id, e1.id), time: interval, color: .green, value: .combined(e0.value, e1.value)))
+        }
     }
+    return result
 }
 
 enum Algorithm: String, CaseIterable, Identifiable {
-    case merge, chain
+    case merge, chain, zip, combineLatest, adjacentPairs
     
     var id: Self { self }
 }
@@ -172,7 +215,9 @@ struct AsyncAlgorithm: View {
     var body: some View {
         VStack {
             TimelineView(events: $sample0, duration: duration)
-            TimelineView(events: $sample1, duration: duration)
+            if algorithm != .adjacentPairs {
+                TimelineView(events: $sample1, duration: duration)
+            }
             TimelineView(events: .constant(results ?? []), duration: duration)
                 .opacity(loading ? 0.5 : 1.0)
                 .animation(.default, value: results)
@@ -187,17 +232,21 @@ struct AsyncAlgorithm: View {
 }
 
 struct AsyncTimelineDemo: View {
+    @State private var selection: Algorithm = .merge
     
     var body: some View {
-        VStack {
-            ForEach(Algorithm.allCases) {
-                Text($0.rawValue)
-                    .padding(4)
-                    .border(.secondary)
-                AsyncAlgorithm(algorithm: $0)
-                Divider()
+        TabView(selection: $selection) {
+            ForEach(Algorithm.allCases) { algorithm in
+                VStack {
+                    Text(algorithm.rawValue)
+                        .font(.largeTitle)
+                    AsyncAlgorithm(algorithm: algorithm)
+                    Divider()
+                }
+                .tag(algorithm)
             }
         }
+        .tabViewStyle(.page)
     }
 }
 
