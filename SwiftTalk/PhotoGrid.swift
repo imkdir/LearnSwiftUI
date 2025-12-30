9ddde9482604c6a3bd2553b18502146186407061
imkdir <hichengd@gmail.com>
2025-12-30 12:46:05 +0800

    add transition observer & unplug MGE behavior

--- SwiftTalk/PhotoGrid.swift
+++ SwiftTalk/PhotoGrid.swift
@@ -7,10 +7,61 @@
 
 import SwiftUI
 
+extension EnvironmentValues {
+    @Entry var transitionIsActive = false
+}
+
+struct TransitionReader<Content: View>: View {
+    @ViewBuilder let content: (Bool) -> Content
+    
+    @Environment(\.transitionIsActive) private var active
+    
+    var body: some View {
+        content(active)
+    }
+}
+
+struct TransitionCoordinator: ViewModifier {
+    var active: Bool
+    
+    func body(content: Content) -> some View {
+        content
+            .environment(\.transitionIsActive, active)
+    }
+}
+
+struct GridCenterPreference: PreferenceKey {
+    static var defaultValue: [Int: CGPoint] = [:]
+    
+    static func reduce(value: inout [Int : CGPoint], nextValue: () -> [Int : CGPoint]) {
+        value.merge(nextValue()) { $1 }
+    }
+}
+
+extension View {
+    func measureGridCenter(_ idx: Int) -> some View {
+        background {
+            GeometryReader { proxy in
+                Color.clear.preference(
+                    key: GridCenterPreference.self,
+                    value: [
+                        idx: proxy.frame(in: .global).center
+                    ]
+                )
+            }
+        }
+    }
+}
+
 struct PhotoGrid: View {
-    @State private var slowAnimation: Bool = true
+    @State private var slowAnimation: Bool = false
     @State private var detail: Int?
-    @Namespace private var grid
+    
+    @Namespace private var transition
+    @Namespace private var idle
+    
+    @State private var gridCenterMap: [Int: CGPoint] = [:]
+    @State private var detailCenter: CGPoint?
     
     var body: some View {
         ZStack {
@@ -20,26 +71,41 @@ struct PhotoGrid: View {
                         let opacity = gridOpacity(atIndex: idx)
                         Image("beach_\(idx)")
                             .resizable()
-                            .matchedGeometryEffect(id: idx.description, in: grid, isSource: detail == nil)
+                            .measureGridCenter(idx)
+                            .matchedGeometryEffect(id: idx.description, in: transition)
                             .aspectRatio(1, contentMode: .fit)
                             .onTapGesture {
-                                detail = idx
+                                    detail = idx
                             }
                             .opacity(opacity)
                             .animation(.default.speed(speed), value: opacity == 0)
                     }
                 }
             }
+            .onPreferenceChange(GridCenterPreference.self) {
+                gridCenterMap = $0
+            }
             if let detail {
-                Image("beach_\(detail)")
-                    .resizable()
-                    .matchedGeometryEffect(id: detail.description, in: grid)
-                    .aspectRatio(contentMode: .fit)
-                    .offset(offset)
-                    .scaleEffect(dragScale)
-                    .animation(.spring.speed(speed), value: offset == .zero)
-                    .gesture(detailGesture)
-                    .id(detail)
+                ZStack {
+                    TransitionReader { active in
+                        let namespace = active ? transition : idle
+                        Image("beach_\(detail)")
+                            .resizable()
+                            .onGeometryChange(for: CGPoint.self, of: {
+                                $0.frame(in: .global).center
+                            }, action: {
+                                detailCenter = $0
+                            })
+                            .matchedGeometryEffect(id: detail.description, in: namespace, isSource: false)
+                            .aspectRatio(contentMode: .fit)
+                            .offset(offset)
+                            .scaleEffect(dragScale)
+                            .gesture(detailGesture)
+                    }
+                }
+                .zIndex(2)
+                .id(detail)
+                .transition(.modifier(active: TransitionCoordinator(active: true), identity: TransitionCoordinator(active: false)))
             }
         }
         .animation(.default.speed(speed), value: detail)
@@ -59,7 +125,7 @@ struct PhotoGrid: View {
         slowAnimation ? 0.2 : 1.0
     }
     
-    @GestureState private var offset: CGSize = .zero
+    @State private var offset: CGSize = .zero
     
     private var dragScale: Double {
         guard offset.height > 0 else { return 1.0 }
@@ -77,15 +143,18 @@ struct PhotoGrid: View {
             detail = nil
         }
         let drag = DragGesture()
-            .updating($offset) { value, state, _ in
-                state = value.translation
-            }
+            .onChanged({ value in
+                offset = value.translation
+            })
             .onEnded { value in
-                if value.velocity.height > 0 {
-                    detail = nil
+                withAnimation(.interpolatingSpring(mass: 1, stiffness: 200, damping: 100, initialVelocity: 0).speed(speed)) {
+                    offset = .zero
+                    if value.velocity.height > 0 {
+                        detail = nil
+                    }
                 }
             }
-        return drag.exclusively(before: tap)
+        return drag.simultaneously(with: tap)
     }
 }
 
