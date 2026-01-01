7076ea377660968442c5eccc97845424c7198ec0
imkdir <hichengd@gmail.com>
2026-01-01 15:22:40 +0800

    #333 staggered animations w/ animatable

--- SwiftTalk/StaggeredAnimations.swift
+++ SwiftTalk/StaggeredAnimations.swift
@@ -13,7 +13,7 @@ struct MenuItemStyle: LabelStyle {
         HStack {
             configuration.title
             configuration.icon
-                .frame(width: 40, height: 40)
+                .frame(width: 50, height: 50)
                 .background {
                     Circle().fill(Color(uiColor: .systemGray6))
                 }
@@ -34,7 +34,7 @@ extension HorizontalAlignment {
 }
 
 extension View {
-    func variadic<R: View>(_ transform: @escaping (_VariadicView.Children) -> R) -> some View {
+    func variadic<R: View>(@ViewBuilder _ transform: @escaping (_VariadicView.Children) -> R) -> some View {
         _VariadicView.Tree(Helper(transform: transform)) {
             self
         }
@@ -49,17 +49,35 @@ private struct Helper<R: View>: _VariadicView.MultiViewRoot {
     }
 }
 
-struct StaggeredStack<Content: View>: View {
-    let open: Bool
-    var speed: Double = 0.1
-    @ViewBuilder let content: Content
+struct StaggeredStack<Content: View>: View, Animatable {
+    let content: Content
+    let reversed: Bool
+    
+    private var progress: CGFloat
+    
+    var animatableData: CGFloat {
+        get { progress }
+        set { progress = newValue }
+    }
+    
+    init(open: Bool, reversed: Bool = true, @ViewBuilder content: () -> Content) {
+        self.progress = open ? 1 : 0
+        self.content = content()
+        self.reversed = reversed
+    }
+    
+    func opacity(at index: Int, count: Int) -> Double {
+        let duration = 1.0 / Double(count)
+        let position = reversed ? count - 1 - index : index
+        let start = Double(position) / Double(count)
+        return max(0, min(1, (progress - start) / duration))
+    }
     
     var body: some View {
         content.variadic { children in
             ForEach(children.enumerated(), id: \.offset) { (offset, item) in
-                let delay = open ? children.count - 1 - offset : offset
                 item
-                    .staggered(open: open, delay: Double(delay) * speed)
+                    .opacity(opacity(at: offset, count: children.count))
             }
         }
     }
@@ -120,7 +138,7 @@ struct StaggeredMenu: View {
         }
     }
     
-    @State private var open: Bool = true
+    @State private var open: Bool = false
     
     var body: some View {
         VStack(alignment: .menu) {
@@ -130,6 +148,7 @@ struct StaggeredMenu: View {
                         .labelStyle(.menuItem)
                 }
             }
+            .animation(.bouncy.speed(0.5), value: open)
             Button {
                 open.toggle()
             } label: {
